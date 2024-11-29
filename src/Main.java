8bfbb667b0af9361a720a5f5348571570ef473cf
Arthur Santos <gaqtto@gmail.com>
2024-11-29 00:23:54 -0300

    Atividade 1 e 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,83 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int tamanho = 10;
+        int[] pilha = new int[tamanho];
+        int topo = -1;
+
+        //aplicação console de exemplo sobre pilhas :)
+        System.out.println("Iniciando processo de carga da pilha...");
+
+        topo = push(pilha, topo, 4);
+        topo = push(pilha, topo, 12);
+        topo = push(pilha, topo, 23);
+        topo = push(pilha, topo, 8);
+
+        System.out.println("Conteúdo da pilha após empilhamento: ");
+        imprimirPilha(pilha, topo);
+
+        System.out.println("Retirando dois itens da pilha...");
+        topo = pop(pilha, topo);
+        topo = pop(pilha, topo);
+
+        System.out.println("\nConteúdo da pilha após retirada:");
+        imprimirPilha(pilha, topo);
+
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        //exemplo sobre lista :)
+        System.out.println("Iniciando processo da lista FIFO...");
+        LinkedList<Integer> listaFila = new LinkedList<>();
+
+        // Adicionando elementos na fila
+        listaFila.addLast(5);
+        listaFila.addLast(23);
+        listaFila.addLast(2);
+        System.out.println("Lista encadeada após adicionar elementos: " + listaFila);
+
+        System.out.println("removendo...");
+        int removido1 = listaFila.removeFirst();
+        System.out.println("Elemento removido: " + removido1);
+        System.out.println("Lista encadeada após a primeira remoção: " + listaFila);
+
+        int removido2 = listaFila.removeFirst();
+        System.out.println("Elemento removido: " + removido2);
+        System.out.println("Lista encadeada  após a segunda remoção: " + listaFila);
+    }
+
+
+    public static int push(int[] pilha, int topo, int valor) {
+        if (topo < pilha.length - 1) {
+            topo++;
+            pilha[topo] = valor;
+            System.out.println("Empilhado: " + valor);
+        } else {
+            System.out.println("Pilha cheia. Não é possível empilhar " + valor);
+        }
+        return topo;
+    }
+
+    public static int pop(int[] pilha, int topo) {
+        if (topo >= 0) {
+            int valorRemovido = pilha[topo];
+            topo--;
+            System.out.println("Desempilhado: " + valorRemovido);
+        } else {
+            System.out.println("Pilha vazia. Não é possível desempilhar.");
+        }
+        return topo;
+    }
+
+    public static void imprimirPilha(int[] pilha, int topo) {
+        if (topo == -1) {
+            System.out.println("Não há itens na pilha.");
+        } else {
+            System.out.print("Pilha: ");
+            for (int i = 0; i <= topo; i++) {
+                System.out.print(pilha[i] + " ");
+            }
+            System.out.println();
         }
     }
 }
